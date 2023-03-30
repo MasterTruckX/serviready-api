e3d8ee54bc73571c676771d5d28a40e8ab383af2
unknown <jorge.orozco@cetys.edu.mx>
2023-03-29 18:09:13 -0700

    Feat: Data Services added

--- src/services/waiters/index.js
+++ src/services/waiters/index.js
@@ -15,8 +15,63 @@ class WaiterServices {
     }
 
     create(newWaiter){
-        // return
+        return new Promise((resolve,reject) => {
+            setTimeout(() => {
+                this.waiters.push(newWaiter);
+                resolve();
+            },1000);
+        });
     }
+
+    queryAll(){
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(this.waiters); // try
+                // reject catch
+            }, 1000);
+        });
+    }
+
+    getById(id){
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                const selectedWaiter = this.waiters.filter(waiter => waiter.id === parseInt(id) )[0];
+                resolve(selectedWaiter);
+            }, 1000);
+        });
+    }
+
+    editPartial(id, property, value){
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                const index = this.waiters.findIndex(waiter => waiter.id === parseInt(id));
+                var waiter = this.waiters[index];
+                waiter[property] = value;
+                resolve();
+            }, 1000);
+        });
+    }
+
+    editComplete(id, waiter){
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                const index = this.waiters.findIndex(waiter => waiter.id === parseInt(id));
+                this.waiters[index] = waiter;
+                resolve();
+            }, 1000);
+        });
+    }
+
+    del(id){
+        return new Promise((resolve,reject) => {
+            setTimeout(() => {
+                const index = this.waiters.findIndex(waiter => waiter.id === parseInt(id));
+                this.waiters.splice(index,1);
+                resolve();
+            },1000)
+        });
+    }
+
 }
 
 module.exports = WaiterServices;
