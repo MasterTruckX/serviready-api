a0b81d6a98ecd0457da03cd77ad6aed7c4564410
unknown <jorge.orozco@cetys.edu.mx>
2023-03-29 21:59:44 -0700

    Feat: Postman collection uploaded and functional

--- src/services/waiters/index.js
+++ src/services/waiters/index.js
@@ -9,8 +9,8 @@ class WaiterServices {
     generateData(){
         this.waiters = [
             {id:1, name: 'Ayla', lastName: 'Gooden', wage: 13.5, workDays: ['THU','FRI','SAT','SUN','MON']},
-            {id:2, name: 'Marcello', lastName: 'Pagano', wage: 13, workDays: [,'FRI','SAT','MON','TUE']},
-            {id:2, name: 'Masha', lastName: 'Stancic', wage: 14.5, workDays: ['WED','FRI','SAT','SUN','MON']}
+            {id:2, name: 'Marcello', lastName: 'Pagano', wage: 13, workDays: ['FRI','SAT','MON','TUE']},
+            {id:3, name: 'Masha', lastName: 'Stancic', wage: 14.5, workDays: ['WED','FRI','SAT','SUN','MON']}
         ]
     }
 
